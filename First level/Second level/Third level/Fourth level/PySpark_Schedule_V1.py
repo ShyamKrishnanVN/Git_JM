b19c7015c24ce7da01f6d9da5cd8b5ab006bcca9
Shyam Krishnan <vnshyamkrishnan@gmail.com>
2023-11-08 10:20:54 +0000

    Upload New File

---
+++ First level/Second level/Third level/Fourth level/PySpark_Schedule_V1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+def input_fun():
+    from Notebook.DSNotebook.NotebookExecutor import NotebookExecutor
+    nb = NotebookExecutor()
+    df_Time_Series_Test = nb.get_data('17171689931251954', '@SYS.USERID', 'True', {}, [], None, sparkSession)
+    return df_Time_Series_Test.show(10)
+
+# In[8]:
+
+
+input_fun()
