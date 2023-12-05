e33a68f7a2c19199eade6fd26c418e5674a5ff97
ShyamKrishnanVN <ShyamKrishnanVN>
2023-12-05 15:15:07 +0530

    Folder level

--- First level/Second level/Third level/Fourth level/PySpark_Schedule_V1.py
+++ First level/Second level/Third level/Fourth level/PySpark_Schedule_V1.py
@@ -2,9 +2,5 @@ def input_fun():
     from Notebook.DSNotebook.NotebookExecutor import NotebookExecutor
     nb = NotebookExecutor()
     df_Time_Series_Test = nb.get_data('17171689931251954', '@SYS.USERID', 'True', {}, [], None, sparkSession)
-    return df_Time_Series_Test.show(10)
-
-# In[8]:
-
-
+    return df_Time_Series_Test.show(11)
 input_fun()
