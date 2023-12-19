031776d041ceb53bac64c171615537994f98e9ff
ShyamKrishnanVN <146066410+ShyamKrishnanVN@users.noreply.github.com>
2023-12-19 12:24:11 +0530

    Update PySpark_Schedule_V1.py

--- First level/Second level/Third level/Fourth level/PySpark_Schedule_V1.py
+++ First level/Second level/Third level/Fourth level/PySpark_Schedule_V1.py
@@ -1,6 +1,6 @@
 def input_fun():
     from Notebook.DSNotebook.NotebookExecutor import NotebookExecutor
     nb = NotebookExecutor()
-    df_Time_Series_Test = nb.get_data('17171689931251954', '@SYS.USERID', 'True', {}, [], None, sparkSession)
+    df_Time_Series_Test2 = nb.get_data('17171689931251954', '@SYS.USERID', 'True', {}, [], None, sparkSession)
     return df_Time_Series_Test.show(11)
 input_fun()
