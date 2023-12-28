b438f4e142ba4aaea17caa0e8d052403e015a745
ShyamKrishnanVN <ShyamKrishnanVN>
2023-12-28 12:47:49 +0530

    Python file_new

---
+++ For_utils/Sample python file.py
@@ -0,0 +1,7 @@
+from sklearn.preprocessing import LabelEncoder
+import pandas as pd
+
+def label_encoder(df, input_col, output_col):
+    le = LabelEncoder()
+    df[output_col] = le.fit_transform(df[input_col]);
+    return df
